55de363ecf256186de765740aa03889667b36548
Ole Kristian Sandum <oksandum@gmail.com>
2017-11-05 23:53:03 +0100

    font: fira code

--- init.el
+++ init.el
@@ -20,4 +20,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Fira Code" :foundry "CTDB" :slant normal :weight normal :height 128 :width normal)))))
