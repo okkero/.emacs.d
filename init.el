98831be446854e58b4b58ed1c39d8b2ec17ad444
Ole Kristian Sandum <ole.kristian.sandum@iwmac.no>
2018-03-01 17:04:07 +0100

    nasm mode

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
  '(custom-enabled-themes (quote (wombat)))
  '(package-selected-packages
    (quote
-    (csharp-mode dotnet racer intero company-irony irony yaml-mode magit rust-mode crystal-mode haskell-mode fsharp-mode idris-mode elm-mode))))
+    (nasm-mode csharp-mode dotnet racer intero company-irony irony yaml-mode magit rust-mode crystal-mode haskell-mode fsharp-mode idris-mode elm-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
